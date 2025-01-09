1e8e2b6fa157826079dd199dd2d5fb408d2be388
Errabi Mohamed Amine <errabi.ami@gmail.com>
2025-01-09 16:32:03 +0100

    Structure project

---
+++ src/test/java/com/errabi/productmgt/ProductMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.errabi.productmgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMgtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
